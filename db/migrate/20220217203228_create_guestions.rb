6bcb90a20d75167c1f23c80ad786021571bdcdfb
katelarionova2058 <2dvedma@gmail.com>
2022-02-19 00:04:33 +0300

    новый файл:    app/models/answer.rb 	новый файл:    app/models/category.rb 	новый файл:    app/models/guestion.rb 	новый файл:    app/models/test.rb 	новый файл:    app/models/user.rb 	новый файл:    db/migrate/20220215220058_create_tests.rb 	новый файл:    db/migrate/20220215235112_create_users.rb 	новый файл:    db/migrate/20220217203228_create_guestions.rb 	новый файл:    db/migrate/20220217203404_create_answers.rb 	новый файл:    db/migrate/20220217203640_create_categories.rb         новый файл:    db/migrate/20220217221102_add_default_value.rb 	новый файл:    db/schema.rb 	новый файл:    test/fixtures/answers.yml 	новый файл:    test/fixtures/categories.yml 	новый файл:    test/fixtures/guestions.yml 	новый файл:    test/fixtures/tests.yml 	новый файл:    test/fixtures/users.yml 	новый файл:    test/models/answer_test.rb 	новый файл:    test/models/category_test.rb 	новый файл:    test/models/guestion_test.rb 	новый файл:    test/models/test_test.rb 	новый файл:    test/models/user_test.rb
    new lesson

---
+++ db/migrate/20220217203228_create_guestions.rb
@@ -0,0 +1,9 @@
+class CreateGuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :guestions do |t|
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/guestion_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GuestionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
