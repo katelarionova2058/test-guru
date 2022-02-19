90bc03adf767b7d146237ec01427dc6e608aae1b
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2022-02-20 01:04:08 +0300

    Delete 20220217203228_create_guestions.rb

--- 20220217203228_create_guestions.rb
+++
@@ -1,9 +0,0 @@
-class CreateGuestions < ActiveRecord::Migration[7.0]
-  def change
-    create_table :guestions do |t|
-      t.text :body, null: false
-      t.references :test, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
