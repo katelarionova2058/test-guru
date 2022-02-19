a451ca5c398f1fca8d39e3e8079fc0f20a28c13e
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2022-02-20 01:23:28 +0300

    Update 20220217203640_create_categories.rb

--- db/migrate/20220217203640_create_categories.rb
+++ db/migrate/20220217203640_create_categories.rb
@@ -1,8 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.text :title
-
+      t.text :title, null: false
       t.timestamps
     end
   end
