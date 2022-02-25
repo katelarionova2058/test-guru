304f056af42ff5690baf2d3dca0d229663a8cd6a
katelarionova2058 <2dvedma@gmail.com>
2022-02-25 22:31:27 +0300

    update app/models/user.rb, delete test/models/question_test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   def find_user_tests_by_level(level)
     Test.where(level: level)
         .joins('JOIN user_tests ON user_tests.test_id = tests.id')
-        .where(user_tests: { user_id: id })
+        .where(tests: { user_id: id })
   end
 end
