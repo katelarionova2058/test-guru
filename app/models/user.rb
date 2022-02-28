70668d910aaa70b592aca64a19c3b204d30d8026
katelarionova2058 <2dvedma@gmail.com>
2022-02-28 17:47:28 +0300

    commit3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def find_user_tests_by_level(level)
     Test.where(level: level)
-        .joins('JOIN tests ON tests.test_id = tests.id')
-        .where(tests: { user_id: id })
+        .joins('JOIN tests ON tests_users.test_id = tests.id')
+        .where(tests_users:{ user_id: id })
   end
 end
