d552f8799d32b7841063b179dc3dfda0de3ad915
katelarionova2058 <2dvedma@gmail.com>
2022-03-01 12:12:54 +0300

    commit4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def find_user_tests_by_level(level)
     Test.where(level: level)
-        .joins('JOIN tests ON tests_users.test_id = tests.id')
+        .joins('JOIN tests_users ON tests_users.test_id = tests.id')
         .where(tests_users:{ user_id: id })
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,26 +12,26 @@ users = User.create!([{ name: 'User1', email: 'user1@mail.ru', password: '123use
 
 categories = Category.create!([{ title: 'Category_1' }, { title: 'Category_2' }, { title: 'Категория_3' }])
 
-tests = Test.create([{title: 'Test1', level: 0, category_id: categories[1].id, author_id: users[0].id },
+tests = Test.create!([{title: 'Test1', level: 0, category_id: categories[1].id, author_id: users[0].id },
                      {title: 'Test2', level: 1, category_id: categories[1].id, author_id: users[1].id },
                      {title: 'Test3', level: 0, category_id: categories[0].id, author_id: users[2].id }])
 
-questions = Question.create([{body: 'Question1_Test1', test_id: tests[0].id},
+questions = Question.create!([{body: 'Question1_Test1', test_id: tests[0].id},
                              {body: 'Question2_Test1', test_id: tests[0].id},
                              {body: 'Question1_Test2', test_id: tests[1].id},
                              {body: 'Question2_Test2', test_id: tests[1].id},
                              {body: 'Question1_Test3', test_id: tests[2].id},
                              {body: 'Question2_Test3', test_id: tests[2].id}])
 
-answers = Answer.create([{answer: "Answer1_Question1_Test1",question_id: questions[0].id, correct: true},
-                         {answer: "Answer2_Question1_Test1",question_id: questions[0].id, correct: false},
-                         {answer: "Answer1_Question2_Test1",question_id: questions[1].id, correct: true},
-                         {answer: "Answer1_Question2_Test1",question_id: questions[1].id, correct: false},
-                         {answer: "Answer1_Question1_Test2",question_id: questions[2].id, correct: true},
-                         {answer: "Answer2_Question1_Test2",question_id: questions[2].id, correct: false},
-                         {answer: "Answer1_Question2_Test2",question_id: questions[3].id, correct: true},
-                         {answer: "Answer1_Question2_Test2",question_id: questions[3].id, correct: false},
-                         {answer: "Answer1_Question1_Test3",question_id: questions[4].id, correct: true},
-                         {answer: "Answer2_Question1_Test3",question_id: questions[4].id, correct: false},
-                         {answer: "Answer1_Question2_Test3",question_id: questions[5].id, correct: true},
-                         {answer: "Answer1_Question2_Test3",question_id: questions[5].id, correct: false}])
+# answers = Answer.create!([{answer: "Answer1_Question1_Test1",question_id: questions[0].id, correct: true},
+#                          {answer: "Answer2_Question1_Test1",question_id: questions[0].id, correct: false},
+#                          {answer: "Answer1_Question2_Test1",question_id: questions[1].id, correct: true},
+#                          {answer: "Answer1_Question2_Test1",question_id: questions[1].id, correct: false},
+#                          {answer: "Answer1_Question1_Test2",question_id: questions[2].id, correct: true},
+#                          {answer: "Answer2_Question1_Test2",question_id: questions[2].id, correct: false},
+#                          {answer: "Answer1_Question2_Test2",question_id: questions[3].id, correct: true},
+#                          {answer: "Answer1_Question2_Test2",question_id: questions[3].id, correct: false},
+#                          {answer: "Answer1_Question1_Test3",question_id: questions[4].id, correct: true},
+#                          {answer: "Answer2_Question1_Test3",question_id: questions[4].id, correct: false},
+#                          {answer: "Answer1_Question2_Test3",question_id: questions[5].id, correct: true},
+#                          {answer: "Answer1_Question2_Test3",question_id: questions[5].id, correct: false}])
