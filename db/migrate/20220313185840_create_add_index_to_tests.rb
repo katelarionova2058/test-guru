d2eca62bb92f54ab969181082b1fd242245c8495
katelarionova2058 <2dvedma@gmail.com>
2022-03-13 22:01:30 +0300

    correct2

--- db/migrate/20220313185840_create_add_index_to_tests.rb
+++ db/migrate/20220313185840_create_add_index_to_tests.rb
@@ -1,7 +1,5 @@
 class CreateAddIndexToTests < ActiveRecord::Migration[7.0]
-  
   def change
-    add_index :tests , [ :title , :level ] , unique:  true
+     add_index :tests , [ :title , :level ] , unique:  true
   end
-end
 end
